dfea303a831f9ab5da23d3247524fd614614cefb
Ben Arthur <bjarthur70@gmail.com>
2026-07-06 12:35:07 -0400

    hoist include_lazy_artifacts to command line flag (#134)

--- src/bundling.jl
+++ src/bundling.jl
@@ -24,7 +24,8 @@ function bundle_products(recipe::BundleRecipe)
     stdlibs = unique(vcat(PackageCompiler.gather_stdlibs_project(ctx2),
                           intersect(PackageCompiler._STDLIBS, map(x->x.name, Base._sysimage_modules))))
     libs_info = PackageCompiler.bundle_julia_libraries(recipe.output_dir, stdlibs; quiet)
-    artifacts_info = PackageCompiler.bundle_artifacts(ctx2, recipe.output_dir; include_lazy_artifacts=false, quiet) # Lazy artifacts
+    artifacts_info = PackageCompiler.bundle_artifacts(ctx2, recipe.output_dir;
+            include_lazy_artifacts=recipe.bundle_lazy_artifacts, quiet) # Lazy artifacts
     PackageCompiler.bundle_cert(recipe.output_dir) # SSL certificates
 
     # Re-home bundled libraries into the desired bundle layout
